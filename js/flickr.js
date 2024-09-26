c92fa39577dc0045ca07264af030cde879873b15
khi8194 <khi8194@gmail.com>
2024-09-26 09:17:50 +0900

    프로필 이미지에 엑박시 대체 이미지 출력

--- js/flickr.js
+++ js/flickr.js
@@ -1,25 +1,41 @@
-const list = document.querySelector(".list");
-const api_key = "21e294ad0ec03a32d7355980457d9e11";
-const baseURL = `https://www.flickr.com/services/rest/?api_key=${api_key}&method=`;
-const myID = "197119297@N02";
-const method_mine = "flickr.people.getPhotos";
-let url = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
-//기본요청URL에 쿼리값 제대로 넣어서 fetch처리했을때
-//콘솔문에 not valid json 오류뜨는 경우
-//원인 : json데이터가 callback함수안에 들어가 있는 경우
-//해결방법 : format=json&nojsoncallback=1 (callback안쪽의 json데이터를 직접 가져옴)
-fetch(url)
-	.then((data) => data.json())
-	.then((json) => {
-		console.log(json.photos.photo);
-		const picArr = json.photos.photo;
-		let tags = "";
+const [btnMine, btnPopular] = document.querySelectorAll("nav button");
 
-		picArr.forEach((pic) => {
-			tags += `
+//스크립트 처음 로드시에는 내갤러리 출력
+fetchFlickr("mine");
+
+//각 버튼 클릭시 갤러리 타입 변경
+btnMine.addEventListener("click", () => fetchFlickr("mine"));
+btnPopular.addEventListener("click", () => fetchFlickr("interest"));
+
+//특정 요소에 특정 함수 연결
+document.body.addEventListener("click", (e) => {
+	if (e.target.className === "thumb") createModal(e);
+	if (e.target.className === "btnClose") removeModal();
+});
+
+//flickr fetching함수
+function fetchFlickr(type) {
+	const list = document.querySelector(".list");
+	const api_key = "21e294ad0ec03a32d7355980457d9e11";
+	const baseURL = `https://www.flickr.com/services/rest/?api_key=${api_key}&method=`;
+	const myID = "197119297@N02";
+	const method_mine = "flickr.people.getPhotos";
+	const method_interest = "flickr.interestingness.getList";
+	let url_mine = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
+	let url_interest = `${baseURL}${method_interest}&nojsoncallback=1&format=json`;
+	let result_url = type === "mine" ? url_mine : url_interest;
+
+	fetch(result_url)
+		.then((data) => data.json())
+		.then((json) => {
+			const picArr = json.photos.photo;
+			let tags = "";
+
+			picArr.forEach((pic) => {
+				tags += `
         <li>
           <figure class='pic'>
-            <img src="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg" alt={pic.title}
+            <img class='thumb' src="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg" alt="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg" />
           </figure>
           <h2>${pic.title}</h2>
           <div class='profile'>
@@ -27,7 +43,38 @@ fetch(url)
           </div>
         </li>
       `;
-		});
+			});
 
-		list.innerHTML = tags;
-	});
+			list.innerHTML = tags;
+			const profilePic = document.querySelectorAll(".profile img");
+			console.log(profilePic);
+			profilePic.forEach(
+				(imgEl) =>
+					(imgEl.onerror = () =>
+						imgEl.setAttribute(
+							"src",
+							"https://www.flickr.com/images/buddyicon.gif"
+						))
+			);
+		});
+}
+
+//모달생성 함수
+function createModal(e) {
+	const imgSrc = e.target.getAttribute("alt");
+
+	const modal = document.createElement("aside");
+	modal.classList.add("modal");
+	modal.innerHTML = `
+      <div class='con'>
+        <img src=${imgSrc} />
+      </div>
+      <button class='btnClose'>CLOSE</button>
+    `;
+	document.body.append(modal);
+}
+
+//모달 제거함수
+function removeModal() {
+	document.querySelector(".modal").remove();
+}
